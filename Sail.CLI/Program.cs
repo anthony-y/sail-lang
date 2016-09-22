078eade8875852c5b90a714692651667c3a4295a
Anthony Baynham <anthonyybayn@gmail.com>
2016-09-22 14:59:05 +0100

    No longer hardcoding filename
    Signed-off-by: Anthony Baynham <anthonyybayn@gmail.com>

--- Sail.CLI/Program.cs
+++ Sail.CLI/Program.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace Sail.CLI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var run = new SailBackend("../../../sample.sail");
+            if (args.Length <= 0)
+                throw new Exception("Expected file name as first argument!");
+
+            var run = new SailBackend(Path.Combine(Environment.CurrentDirectory, args[0]));
         }
     }
 }
